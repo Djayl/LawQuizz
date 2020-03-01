1683cb42321803fdc8998c39b44f21750a269e86
Djayl <djilali.sakkar@gmail.com>
2020-03-01 13:58:01 +0100

    Add GameVC

---
+++ LawQuizz/Controller/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  LawQuizz
+//
+//  Created by MacBook DS on 01/03/2020.
+//  Copyright © 2020 Djilali Sakkar. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
